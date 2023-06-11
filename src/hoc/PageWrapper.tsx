8e31e36ecfe1c4d374c8bcf66f558adc1436974d
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2023-06-11 19:25:02 +0100

    Fixed some errors

--- src/hoc/PageWrapper.tsx
+++ src/hoc/PageWrapper.tsx
@@ -1,8 +1,6 @@
 import React, { ComponentType } from 'react';
 import { motion } from 'framer-motion';
 
-import { pageVariants } from '../utils/motion';
-
 const PageWrapper = <P extends object>(Component: ComponentType<P>) => {
 
   return (props: P) => (
